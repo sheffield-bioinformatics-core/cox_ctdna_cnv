617b716f88f8f32ebc0bcf55089ebe8af02a746e
mattparker <matthewdanielparker@gmail.com>
2018-08-06 14:32:28 +0100

    updating fixed windows generating script

--- window_fixed_generate.pl
+++ window_fixed_generate.pl
@@ -2,26 +2,44 @@
 
 use strict;
 
-open(FH, "<Sample_Pairs.txt");
+my ($rootdir,$scriptdir) = @ARGV;
 
-while (<FH>)
+my $gcbases = "/home/md1mpar/wc/cox_ctdna_cnv/gc1000base_38.txt";
+my $poolbam = "/shared/bioinformatics_core1/Shared/cox/cnv/POOLED_NORMAL/Pool.sorted.final.bam";
 
-   {
+open(FH, "<Samples.txt");
 
-   chomp;
-   (my $sample1, my $sample2, my $name) = split(/\t/, $_, 3);
+while (<FH>){
+
+    chomp;
+    my $sample = $_;
 
    open (OFH, ">window_fixed_$name.sh");
 
    print OFH "#!/bin/bash\n\n";
 
-   print OFH "perl /home/md1jrbx/Shobha/bam2windows.pl --samtools-path /home/md1jrbx/Software/samtools-0.1.19/samtools -ts -cs -d /fastdata/md1jrbx/TMP -w 1000000 -gc /home/md1jrbx/Shobha/gc1000base_38.txt /fastdata/md1jrbx/SHOBHA/BAMS/$sample1.sorted.final.bam /fastdata/md1jrbx/SHOBHA/BAMS/$sample2.sorted.final.bam > /fastdata/md1jrbx/SHOBHA/FIXED_WINDOWS/$name.tab\n";
+   print OFH "#\$ -S /bin/bash\n";
+   print OFH "#\$ -l rmem=10G\n";
+   print OFH "#\$ -m bea\n";
+   print OFH "#\$ -M matthew.parker\@sheffield.ac.uk\n";
+   print OFH "#\$ -l h_rt=4:00:00\n";
+   print OFH "#\$ -o $sample\_fixed_window_generate.out\n";
+   print OFH "#\$ -e $sample\_fixed_window_generate.err\n";
+   print OFH "#\$ -N $sample\n";
+   print OFH "#\$ -pe openmp 1\n\n";
 
-   print OFH "perl /home/md1jrbx/Shobha/clean_tab_fixed.pl $name\n";
+
+   print OFH "module load apps/SAMtools/1.7/gcc-4.9.4\n";
+
+   print OFH "samtools=`which samtools`\n";
+
+   print OFH "perl $scriptdir/bam2windows.pl --samtools-path ${samtools} -ts -cs -d $rootdir/TMP -w 1000000 -gc $gcbases $rootdir/BAMS/$sample.sorted.final.bam $poolbam > $rootdir/FIXED_WINDOWS/sample.tab\n";
+
+   print OFH "perl $scriptdir/clean_tab.pl $rootdir/FIXED_WINDOWS/$sample.tab $rootdir/FIXED_WINDOWS/$sample.bed\n";
 
    close OFH;
 
-   system "qsub window_fixed_$name.sh";
+   #system "qsub -q bioinf-core.q -P bioinf-core window_fixed_$name.sh";
 
    }
 
